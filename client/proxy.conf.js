3ad0789fb6fc8e1c06bee2b0f213ad4ac022a427
JuanCarlosCalleGarcia <Juan.ing.udea@gmail.com>
2017-10-17 13:32:06 -0500

    make it better

--- client/proxy.conf.js
+++ client/proxy.conf.js
@@ -1,12 +1,9 @@
 const PROXY_CONFIG = [
   {
     "context": [
-      "/vendor/assignCoupon",
       "/shop/listProducts",
       "/shop/addProduct",
-      "/shop/listProductsByVendor",
       "/client/addToCart",
-      "/client/listClients",
       "/user/login"
     ],
     "target": "http://localhost:3000",
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,23 +10,19 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AppRoutingModule} from "./app-routing/app-routing.module";
 import { CartListComponent } from './pages/cart-list/cart-list.component';
 import { LoginComponent } from './pages/login/login.component';
-import { WorkstationComponent } from './pages/seller-dashboard/workstation/workstation.component';
-import { InventoryComponent } from './pages/seller-dashboard/inventory/inventory.component';
-import { CouponsComponent } from './pages/coupons/coupons.component';
 
 // librerias
 
-import {RestangularConfigFactory} from './shared/restConfig';
-import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RestangularConfigFactory} from "./shared/restConfig";
 
 // servicios
 
-import { ProductService } from './services/product.service';
+import { ProductService } from "./services/product.service";
 import { ProductFormComponent } from './pages/seller-dashboard/product-form/product-form.component';
-import { CartService } from './services/cart.service';
-import { LoginService } from './services/login.service';
-import { CouponService } from './services/coupon.service';
-
+import { CartService } from "./services/cart.service";
+import { LoginService } from "./services/login.service";
+import { WorkstationComponent } from './pages/seller-dashboard/workstation/workstation.component';
+import { InventoryComponent } from './pages/seller-dashboard/inventory/inventory.component';
 
 @NgModule({
   declarations: [
@@ -36,21 +32,18 @@ import { CouponService } from './services/coupon.service';
     CartListComponent,
     LoginComponent,
     WorkstationComponent,
-    InventoryComponent,
-    CouponsComponent
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RestangularModule.forRoot(RestangularConfigFactory),
-    ReactiveFormsModule,
-    NgbModule.forRoot()
+    ReactiveFormsModule
   ],
   providers: [
     ProductService,
     CartService,
-    LoginService,
-    CouponService
+    LoginService
   ],
   bootstrap: [AppComponent]
 })
